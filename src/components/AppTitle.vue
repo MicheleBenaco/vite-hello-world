352e7f08942e5a616ef8fa16e82a8fb4f8d81089
MicheleBenaco <benaco.michele@yahoo.it>
2022-11-09 16:47:19 +0100

    prova2

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>{{titolo}}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'AppTitle',
+        name: 'AppHero',
+        data() {
+            return {
+                titolo:'La mia prima app con Vite!'
+            }
+        },
+        
+    }
+</script>
+
+<style lang="css" >
+    h1{
+        color: red;
+
+    }
+</style>
